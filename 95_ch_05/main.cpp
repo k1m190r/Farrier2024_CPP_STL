22b7c8976f6332254f7b01934fa9393d22efdc24
k1m190r <k1m190r@outlook.com>
2024-11-24 22:38:02 +0100

    95 Ch 05

--- 95_ch_05/main.cpp
+++ 95_ch_05/main.cpp
@@ -1,19 +1,22 @@
 #include <algorithm>
 #include <array>
+#include <cctype>
 #include <chrono>
 #include <deque>
 #include <execution>
 #include <format>
 #include <functional>
+#include <list>
 #include <numeric>
 #include <print>
 #include <random>
 #include <ranges>
 #include <vector>
 
-using std::print, std::format;
+using std::print, std::format, std::string;
 
 int main() {
+  /***********************************************************************************/
   // 01_array.cpp
   {
     struct Point {
@@ -57,20 +60,21 @@ int main() {
     }
   }
 
+  /***********************************************************************************/
   // 02_vector.cpp
   {
     class Employee {
      public:
-      Employee(int _id, const std::string &_name) : id(_id), name(_name) {}
+      Employee(int _id, const string &_name) : id(_id), name(_name) {}
 
       int getId() const { return id; }
-      const std::string &getName() const { return name; }
+      const string &getName() const { return name; }
 
-      void setName(const std::string &newName) { name = newName; }
+      void setName(const string &newName) { name = newName; }
 
      private:
       int id{0};
-      std::string name;
+      string name;
     };
 
     std::vector<Employee> employees;
@@ -108,6 +112,7 @@ int main() {
     print("Ex2: after erase: {}\n", employees | vtx(p_emp));
   }
 
+  /***********************************************************************************/
   // 03_vector_bool.cpp
   {
     std::vector<bool> boolVec = {true, false, true, true, false};
@@ -136,6 +141,7 @@ int main() {
           boolVec.capacity());
   }
 
+  /***********************************************************************************/
   // 04_deque.cpp
   {
     // demonstrate using a deque as a sliding window over data.
@@ -199,7 +205,7 @@ int main() {
       //   auto sum = std::reduce(policy, v.begin(), v.end());
       //   auto sum = std::accumulate(v.begin(), v.end(), 0);
       //   auto sum = std::ranges::fold_left(v, 0, std::plus<int>());
-      
+
       //   auto make_double = [](auto x) { return (double)x; };
       //   auto op = std::plus<double>{};
       //   auto sum = std::transform_reduce(v.begin(), v.end(), 0, op, make_double);
@@ -209,7 +215,7 @@ int main() {
       print("{} {}\n", sum, elapsed);
     };
 
-    std::vector<int> v(1'000'000'000);
+    std::vector<int> v(1'000'000);
     std::mt19937 gen{std::random_device{}()};
     std::ranges::generate(v, gen);
 
@@ -219,5 +225,151 @@ int main() {
     // measure(exe::unseq, v);
     // measure(exe::par_unseq, v);
     // measure(exe::par, v);
+  }
+
+  /***********************************************************************************/
+  // 05_list.cpp
+  {
+    // auto display = [](auto &lst) { print("Ex05: {}\n", lst); };
+
+    std::list<int> numbers = {5, 1, 8, 3, 7};
+
+    print("Ex05 init: {}\n", numbers);
+    print("Ex05 reverse: {}\n", numbers | std::views::reverse);
+
+    // auto at_8 = std::ranges::find_if(numbers, [](auto x) { return x == 8; });
+    auto at_8 = std::ranges::find(numbers, 8);
+    print("Ex05 from 8 till end: {}\n", std::ranges::subrange(at_8, numbers.end()));
+
+    auto pos = std::ranges::find(numbers, 8);
+    numbers.insert(pos, 2112);
+    print("Ex05 insert after 8: {}\n", numbers);
+
+    numbers.sort();
+    std::list<int> more_numbers = {2, 6, 4};
+    more_numbers.sort();
+    numbers.merge(more_numbers);
+    print("Ex05 after sort and merge: {}\n", numbers);
+
+    std::list<int> additional_numbers = {99, 100, 101};
+    numbers.splice(numbers.end(), additional_numbers);
+    print("Ex05 after splicing: {}\n", numbers);
+
+    numbers.remove_if([](int n) { return n % 2 == 0; });
+    print("Ex05 after removing even: {}\n", numbers);
+  }
+
+  /***********************************************************************************/
+  // 06_forward_list.cpp
+  {
+    // template <typename string, typename int>
+    class ChainedHashTable {
+     public:
+      ChainedHashTable(size_t capacity) : capacity(capacity) { table.resize(capacity); }
+
+      bool get(const string &key, int &value) const {
+        const auto &list = table[hash(key)];
+        for (const auto &bucket : list) {
+          if (bucket.key == key) {
+            value = bucket.value;
+            return true;
+          }
+        }
+        return false;
+      }
+
+      void put(const string &key, const int &value) {
+        auto &list = table[hash(key)];
+        for (auto &bucket : list) {
+          if (bucket.key == key) {
+            bucket.value = value;
+            return;
+          }
+        }
+        list.emplace_front(key, value);
+      }
+
+      bool remove(const string &key) {
+        auto &list = table[hash(key)];
+        list.remove_if([&](const Bucket &bucket) { return bucket.key == key; });
+        return false;
+      }
+
+     private:
+      struct Bucket {
+        string key;
+        int value;
+        Bucket(string k, int v) : key(k), value(v) {}
+      };
+
+      std::vector<std::forward_list<Bucket>> table;
+      size_t capacity;
+      size_t hash(const string &key) const {
+        return std::hash<string>{}(key) % capacity;
+      }
+    };
+
+    ChainedHashTable hashTable(10);
+
+    hashTable.put("apple", 10);
+    hashTable.put("banana", 20);
+    hashTable.put("cherry", 30);
+
+    int value;
+    if (hashTable.get("apple", value)) {
+      print("apple: {}\n", value);
+    }
+
+    if (hashTable.get("banana", value)) {
+      print("banana: {}\n", value);
+    }
+
+    hashTable.remove("banana");
+
+    if (!hashTable.get("banana", value)) {
+      print("no banana\n");
+    }
+  }
+
+  /***********************************************************************************/
+  // 06_string.cpp
+  {
+    string s = "Hello, C++ World!";
+
+    print("Ex06: Size: {}, first char {}\n", s.size(), s[0]);
+
+    string greet = "Hello";
+    string target = "World";
+    string combined = greet + ", " + target + "!";
+    print("Ex06: Combined: {}", combined);
+
+    if (s.find("C++") != string::npos) {
+      print("Ex06: found 'C++'\n");
+    }
+
+    auto to_up = [](auto c) { return std::toupper(c); };
+    auto ups = s | std::views::transform(to_up) | std::ranges::to<string>();
+    print("Ex06: upcase: {}\n", ups);
+
+    auto to_low = [](auto c) { return std::tolower(c); };
+    auto lows = s | std::views::transform(to_low) | std::ranges::to<string>();
+    print("Ex06: lowcase: {}\n", ups);
+
+    std::erase(s, ' ');
+    print("Ex06: erase spaces {}\n", s);
+
+    string first = "apple";
+    string second = "banana";
+
+    if (first < second) {
+      print("Ex06: {}  comes before {}\n", first, second);
+    }
+
+    int number = 2112;
+    string numStr = std::to_string(number);
+    print("Ex06: number as string {}\n", numStr);
+
+    int convertedBack = std::stoi(numStr);
+    print("Ex06: string to num {}\n", convertedBack);
   }
 }
